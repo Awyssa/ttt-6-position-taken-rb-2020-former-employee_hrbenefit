0807eb842197bdcd649872699e8093ca53f7565e
Michael Ernest <m.hendersun@gmail.com>
2020-09-25 12:31:05 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,12 @@
 
 
 def position_taken?(board, index)
-  if board[index] != " " && != "" && != nil
+
+  taken = nil
+  if (board[index] == " " || board[index] == "" || board[index] ==nil)
+     taken = false
+   else
+     taken = true
   end
+  taken
 end
